53a16be10157801cd6a4a5a7770f36a6e182a68c
Jillian Haffner <jillianhaffner1976@gmail.com>
2017-05-19 01:27:01 -0400

    Debugging

--- launch.py
+++ launch.py
@@ -2,11 +2,11 @@
 
 import time
 
+
+def shock(channel, action):
+    io.popen("python3 etgame.py " + channel + " " + action + "")
+
 def main():
-
-     def shock(channel,action):
-        io.popen("python3 etgame.py " + channel + " " + action + "")
-
     while 0=1:
         shock("-c a","-r 250")
         time.sleep( 5 )
