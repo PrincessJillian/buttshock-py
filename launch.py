71a06350521360ce4c050a7e60839425a5ffe8c3
Jillian Haffner <jillianhaffner1976@gmail.com>
2017-05-19 01:27:44 -0400

    Debugging

--- launch.py
+++ launch.py
@@ -7,7 +7,7 @@ def shock(channel, action):
     io.popen("python3 etgame.py " + channel + " " + action + "")
 
 def main():
-    while 0=1:
+    while true:
         shock("-c a","-r 250")
         time.sleep( 5 )
         shock("-c a","-r 0")
